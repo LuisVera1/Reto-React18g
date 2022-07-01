d79160dfff3c5fa5ec4be51b7c12ae730ca84e0d
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-06-30 22:20:40 -0500

    add useState to card

--- src/pages/CreatePost/index.jsx
+++ src/pages/CreatePost/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { create as createUser } from "../../../services/users";
+import { create as createPost } from "../../../services/users";
 
 // Toastify
 import { ToastContainer, toast } from "react-toastify";
@@ -10,6 +10,53 @@ import Input from "../../../components/Input";
 import "./CreatePost.css";
 
 export default function CreatePost() {
+  // Local state
+  //const [autorName, setAutorName] = useState("");
+  const [title, setTitle] = useState("");
+  const [hashtags, setHashtags] = useState("");
+  const [fullPost, setFullPost] = useState("");
+  //const [reactions, setReactions] = useState("");
+  const [imgPost, setImgPost] = useState("");
+  //const [date, setDate] = useState("");
+  //const [coments, setImgComents] = useState("");
+
+  const cleanForm = () => {
+    setTitle("");
+    setHashtags("");
+    setFullPost("");
+    setImgPost("");
+  };
+
+  const isEmpty = (value) => !value;
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (
+      isEmpty(title) ||
+      isEmpty(hashtags) ||
+      isEmpty(fullPost) ||
+      isEmpty(imgPost)
+    ) {
+      toast.error("Llena el form!!!!");
+      return;
+    }
+
+    const data = {
+      title,
+      hashtags,
+      fullPost,
+      imgPost,
+    };
+
+    try {
+      await createPost(data);
+      toast.success("Todo fine!!");
+      cleanForm();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <nav className="navbar navbar-default navbar-fixed-top">
@@ -35,34 +82,42 @@ export default function CreatePost() {
         </div>
       </nav>
       <div className="">
-        <input
+        <Input
           className="form-control form-control-lg"
           name="title"
           placeholder="New post title here..."
           id="floatingTextareaTitle"
-        ></input>
+          value={title}
+          callback={(e) => setTitle(e.target.value)}
+        />
       </div>
       <div className="">
-        <input
+        <Input
           className="form-control"
           name="hastags"
           placeholder="Add up to 4 tags..."
           id="floatingTextareaTags"
-        ></input>
+          value={hashtags}
+          callback={(e) => setHashtags(e.target.value)}
+        />
       </div>
       <div className="">
-        <input
+        <Input
           className="form-control"
           name="fullPost"
           placeholder="Write your post content here..."
           id="floatingTextareaText"
-        ></input>
-        <input
+          value={fullPost}
+          callback={(e) => setFullPost(e.target.value)}
+        />
+        <Input
           className="form-control"
           name="fullPost"
           placeholder="Add an URL link..."
           id="floatingTextareaText"
-        ></input>
+          value={imgPost}
+          callback={(e) => setImgPost(e.target.value)}
+        />
       </div>
 
       <nav>
